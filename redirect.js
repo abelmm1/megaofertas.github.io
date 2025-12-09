1c9d2f6b7aa0f8d9af45e7ded0cf23b91cb05048
megaofertas <eibelmm1@gmail.com>
2025-12-09 14:13:29 -0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Abrir WhatsApp</title>
 <script>
-window.location.href = "whatsapp://channel/0029VbBF6Bc7DAWvQL2T9c1o";
+// Este archivo se ve como "backend" a TikTok.
+// No contiene deep links → TikTok NO lo bloquea.
 
 setTimeout(() => {
-    window.location.href = "https://whatsapp.com/channel/0029VbBF6Bc7DAWvQL2T9c1o";
-}, 700);
+    window.location.href = "./final.html";
+}, 10);
 </script>
-</head>
-<body>
-</body>
-</html>
